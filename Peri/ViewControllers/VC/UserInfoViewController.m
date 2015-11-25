3b87aa17d66ebe1b7ea49f296bdf7a16796657fc
miao445201 <870291020@qq.com>
2015-11-25 10:37:37 +0800

    突然想到的功能点，增加个人中心页面，封面在试图中动画，图片是随机的，当点击之后进入该封面的图库，增加刷新，可以随机从数据库中抽取图片。

--- Peri/ViewControllers/VC/UserInfoViewController.m
+++ Peri/ViewControllers/VC/UserInfoViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setNaviTitle:@"个人中心"];
+    [self animationTest];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,5 +25,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)animationTest {
+    //演员初始化
+    CALayer *groupLayer = [[CALayer alloc] init];
+    groupLayer.frame = CGRectMake(60, 340+100, 50, 50);
+    groupLayer.cornerRadius = 10;
+    groupLayer.backgroundColor = [[UIColor purpleColor] CGColor];
+    [self.view.layer addSublayer:groupLayer];
+    
+    //设定剧本
+    CABasicAnimation *scaleAnimation = [CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    scaleAnimation.fromValue = [NSNumber numberWithFloat:1.0];
+    scaleAnimation.toValue = [NSNumber numberWithFloat:1.5];
+    scaleAnimation.autoreverses = YES;
+    scaleAnimation.repeatCount = MAXFLOAT;
+    scaleAnimation.duration = 0.8;
+    
+    CABasicAnimation *moveAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+    moveAnimation.fromValue = [NSValue valueWithCGPoint:groupLayer.position];
+    moveAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(320 - 80,
+                                                                  groupLayer.position.y)];
+    moveAnimation.autoreverses = YES;
+    moveAnimation.repeatCount = MAXFLOAT;
+    moveAnimation.duration = 2;
+    
+    CABasicAnimation *rotateAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.x"];
+    rotateAnimation.fromValue = [NSNumber numberWithFloat:0.0];
+    rotateAnimation.toValue = [NSNumber numberWithFloat:6.0 * M_PI];
+    rotateAnimation.autoreverses = YES;
+    rotateAnimation.repeatCount = MAXFLOAT;
+    rotateAnimation.duration = 2;
+    
+    CAAnimationGroup *groupAnnimation = [CAAnimationGroup animation];
+    groupAnnimation.duration = 2;
+    groupAnnimation.autoreverses = YES;
+    groupAnnimation.animations = @[moveAnimation, scaleAnimation, rotateAnimation];
+    groupAnnimation.repeatCount = MAXFLOAT;
+    //开演
+    [groupLayer addAnimation:groupAnnimation forKey:@"groupAnnimation"];
+}
 
 @end
